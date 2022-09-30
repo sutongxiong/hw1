57b3961eb998891802732ef377c4830ba06e6ce0
sutongxiong <ericxiong03@gmail.com>
2022-09-30 16:44:51 -0400

    done

--- hw3_q3.py
+++ hw3_q3.py
@@ -1,6 +1,6 @@
 """
 Author: Sutong Xiong
-Assignment / Part: HW3 - Q2
+Assignment / Part: HW3 - Q3
 Date due: 2022-10-06, 11:59pm
 I pledge that I have completed this assignment without
 collaborating with anyone else, in conformance with the
---
+++ hw3_q5.py
@@ -0,0 +1,17 @@
+"""
+Author: Sutong Xiong
+Assignment / Part: HW3 - Q5
+Date due: 2022-10-06, 11:59pm
+I pledge that I have completed this assignment without
+collaborating with anyone else, in conformance with the
+NYU School of Engineering Policies and Procedures on
+Academic Misconduct.
+"""
+
+first_side = float(input("Length of the first side: "))
+second_side = float(input("Length of the second side: "))
+third_side = float(input("Length of the first side: "))
+
+
+
+
