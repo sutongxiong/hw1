0d8f02f244f802a664310b2d454618c181c9d1e5
sutongxiong <ericxiong03@gmail.com>
2022-10-02 10:40:42 -0400

    done

--- hw3_q5.py
+++ hw3_q5.py
@@ -8,10 +8,20 @@ NYU School of Engineering Policies and Procedures on
 Academic Misconduct.
 """
 
-first_side = float(input("Length of the first side: "))
-second_side = float(input("Length of the second side: "))
-third_side = float(input("Length of the first side: "))
+first_side = round(float(input("Length of the first side: ")), 5)
+second_side = round(float(input("Length of the second side: ")), 5)
+third_side = round(float(input("Length of the first side: ")), 5)
+triangle_type = ""
 
-
-
-
+if first_side == second_side == third_side:
+    triangle_type = "an equilateral triangle"
+elif first_side == second_side or second_side == third_side or first_side ==third_side:
+    if first_side**2 == second_side**2*2 or second_side **2 == first_side**2*2 or third_side**2 == first_side**2*2:
+        triangle_type = "an isosceles right triangle"
+    else:
+        triangle_type = "an isosceles triangle that is not a right triangle"
+else:
+    triangle_type = "a triangle that is not an isosceles and not an equilateral (i.e. scalene)"
+first_side = str(first_side)+","
+second_side = str(second_side)+","
+print(first_side, second_side, third_side, "form", triangle_type)
