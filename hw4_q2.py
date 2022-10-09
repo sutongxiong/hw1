f574f56bed8b993650791a1e27065f4b237cafff
sutongxiong <ericxiong03@gmail.com>
2022-10-09 09:56:36 -0400

    done

--- hw4_q2.py
+++ hw4_q2.py
@@ -7,3 +7,12 @@ collaborating with anyone else, in conformance with the
 NYU School of Engineering Policies and Procedures on
 Academic Misconduct.
 """
+
+n = int(input("Enter the number of lines composed of half of the asterisks: "))
+for i in range(n+1):
+    print(" "*i + "*"*(2*n-2*i+1)+" "*i)
+for i in range(n):
+    print(" "*(n-i-1) + "*"*(i*2+2*n-5) + " " * (n-i))
+
+
+
