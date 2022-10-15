37156443162299c80582ebd6d9209f8764c394cd
sutongxiong <ericxiong03@gmail.com>
2022-10-14 23:12:28 -0400

    done

--- hw4_q5.py
+++ hw4_q5.py
@@ -16,14 +16,14 @@ richest_player = 0
 for player_number in range(1,total_players+1):
     total_property_value = 0.0
 
-    property_value = float(input("Enter the value of a property/asset, or DONE to finish: "))
+    property_value = input("Enter the value of a property/asset, or DONE to finish: ")
     while property_value != "DONE":
 
         total_property_value += float(property_value)
         property_value = input("Enter the value of a property/asset, or DONE to finish: ")
     print("Player", player_number, "has", round(total_property_value,2), "dollars")
     if highest_sum < total_property_value:
-        highest_sum = total_property_value
+        highest_sum = round(total_property_value,2)
         richest_player = player_number
 
 print(richest_player, "wins with",highest_sum, "dollars!")
---
+++ hw5_q1.py
@@ -0,0 +1,13 @@
+"""
+Author: Sutong Xiong
+Assignment / Part: HW1 - Q1
+Date due: 2022-10-20, 11:59pm
+I pledge that I have completed this assignment without
+collaborating with anyone else, in conformance with the
+NYU School of Engineering Policies and Procedures on
+Academic Misconduct.
+"""
+
+
+
+
